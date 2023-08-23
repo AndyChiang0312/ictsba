384bb3d568eae784c65f45b0a188dbf68841cae9
AndyChiang0312 <89000322+AndyChiang0312@users.noreply.github.com>
2023-08-23 23:27:16 +0800

    Delete slos directory

--- slos/js/index.js
+++
@@ -1,9 +0,0 @@
-const date = new Date();
-
-let day = date.getDate();
-let month = date.getMonth() + 1;
-let year = date.getFullYear();
-
-
-document.getElementById('date').innerHTML = year + "年" + month + "月" + day + "日";
-document.getElementById('thisMonth').innerHTML = month + "月訂餐";
