d20bf3fa72e63910c58f588f882b0703dd182720
AndyChiang0312 <89000322+AndyChiang0312@users.noreply.github.com>
2023-08-31 11:51:26 +0800

    Add files via upload

---
+++ slos/js/index.js
@@ -0,0 +1,11 @@
+var year = new Date().getFullYear();
+var month = new Date().getMonth() + 1;
+var date = new Date().getDate();
+
+function confirmLogout() {
+    if(window.confirm('確定要登出嗎？')){
+        window.open('../login.html', '_self');
+    }
+}
+
+document.getElementById('todayDate').innerHTML = String(year) + '年' + String(month) + '月' + String(date) + '日';
