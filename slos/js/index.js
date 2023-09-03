c7fed0bbd79d00c883ab9f69c27c63558bd0da7a
AndyChiang0312 <89000322+AndyChiang0312@users.noreply.github.com>
2023-09-03 14:00:38 +0800

    Update index.js

--- slos/js/index.js
+++ slos/js/index.js
@@ -2,7 +2,7 @@ var year = new Date().getFullYear();
 var month = new Date().getMonth() + 1;
 var date = new Date().getDate();
 var day = new Date(String(month) + '/' + String(date) + '/' + String(year)).getDay();
-var chineseWeekday = ['一','二','三','四','五', '六', '日'];
+var chineseWeekday = ['日', '一','二','三','四','五','六'];
 
 function confirmLogout() {
     if(window.confirm('確定要登出嗎？')){
@@ -10,4 +10,4 @@ function confirmLogout() {
     }
 }
 
-document.getElementById('todayDate').innerHTML = String(year) + '年' + String(month) + '月' + String(date) + '日(星期' + String(chineseWeekday[day - 1]) + ')';
+document.getElementById('todayDate').innerHTML = String(year) + '年' + String(month) + '月' + String(date) + '日(星期' + String(chineseWeekday[day]) + ')';
