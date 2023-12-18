dd736712395623b89818b777817aafe19037075b
AndyChiang0312 <89000322+AndyChiang0312@users.noreply.github.com>
2023-12-18 22:39:39 +0800

    Delete slos directory

--- slos/js/mealDetails.js
+++
@@ -1,3 +0,0 @@
-document.getElementById('mealName').innerHTML = window.sessionStorage.getItem('meal');
-
-document.getElementById('mealPhotoInDetail').innerHTML = '<img src=' + 'media/photos/meals/' + window.sessionStorage.getItem('meal') + '.jpg>';
