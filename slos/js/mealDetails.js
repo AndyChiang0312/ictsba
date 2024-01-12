1cedc6ea3e128d984a8fa3318de3d6dce78c5078
AndyChiang0312 <89000322+AndyChiang0312@users.noreply.github.com>
2024-01-12 13:31:15 +0800

    Add files via upload

---
+++ slos/js/mealDetails.js
@@ -0,0 +1,3 @@
+document.getElementById('mealName').innerHTML = window.sessionStorage.getItem('meal');
+
+document.getElementById('mealPhotoInDetail').innerHTML = '<img src=' + 'media/photos/meals/' + window.sessionStorage.getItem('meal') + '.jpg alt="' + window.sessionStorage.getItem('meal') + '">';
